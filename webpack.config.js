8e037bd8ac0635e8a82a2843408fb36b879084bf
Et7f3 <cadeaudeelie@gmail.com>
2022-06-07 02:31:31 +0200

    fix: Update PluginWebpackConfig to new API

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
 const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
 const path = require('path');
 
-module.exports = new PluginWebpackConfig('org.graylog.plugins.sample.SamplePlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig(path.resolve(__dirname), 'org.graylog.plugins.sample.SamplePlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
   // Here goes your additional webpack configuration.
 });
